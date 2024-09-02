20305652eaff9b4b375587277f009b2d3503aa38
RC_Student_lab <RC_Student_lab@labG9AEB3>
2024-09-02 10:54:08 +0000

    Add project files.

---
+++ StudentApp/Models/Course.cs
@@ -0,0 +1,20 @@
+﻿//Will sho the table for us
+using System.ComponentModel.DataAnnotations;
+
+namespace StudentApp.Models
+
+{
+
+    public class Course
+    {
+        public int Id { get; set; }
+
+        [MaxLength(100)] //Length for thr name field
+        public string Name { get; set; } = "";
+        public string Description { get; set; } = "";
+
+        [MaxLength(100)]
+        public string ModuleName { get; set; } = "";
+
+    }
+}
---
+++ StudentApp/Services/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentApp.Models;
+
+
+
+namespace StudentApp.Services
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        //our table
+        public DbSet<Course> Courses { get; set; }  
+    }
+}
